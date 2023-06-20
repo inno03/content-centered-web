033c32c12c70157e59e4364d714da866010af777
Inno <innocentogayi01@gmail.com>
2023-06-20 23:52:50 +0300

    first commit

---
+++ studio/schemas/schema.js
@@ -0,0 +1,49 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import blockContent from './blockContent'
+import { DocumentIcon } from '@sanity/icons'
+
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    {
+      title: 'Post',
+      name: 'post',
+      icon: DocumentIcon,
+      type: 'document',
+      fields: [
+        {
+          name: 'title',
+          title: 'Title',
+          type: 'string',
+        },
+        {
+          name: 'slug',
+          title: 'Slug',
+          type: 'slug',
+          options: {
+            source: 'title',
+            maxLength: 96,
+          },
+        },
+        {
+          name: 'publishedAt',
+          title: 'Published at',
+          type: 'datetime',
+        },
+        {
+          name: 'body',
+          title: 'Body',
+          type: 'blockContent',
+        },
+      ],
+      preview: {
+        select: {
+          title: 'title',
+        },
+      },
+    },
+    blockContent,
+  ]),
+})
